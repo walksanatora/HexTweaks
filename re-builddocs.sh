5450eddbc4bd5395f0f1c8c595daaa9bd46bf882
walksanatora <walkerffo22@gmail.com>
2023-04-21 16:29:34 -0700

    updated docs

---
+++ re-builddocs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+set cwd "$PWD"
+echo $cwd
+cd ../HexTweaks
+hexcasting_root=../HexMod/ python3 doc/collate_data.py common/src/main/resources common/src/main/java hextweaks thetweakedbook doc/template.html doc/docs.html doc/data.json
+cd "$cwd"
+cp ../HexTweaks/doc/docs.html index.html
+git add -A
+git commit -m "updated docs"
+git push
