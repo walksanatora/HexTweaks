885adc0f8b66de7582ed93187ef2976fd05f59f0
walksanatora <walkerffo22@gmail.com>
2023-05-09 08:55:06 -0700

    updated docs

--- re-builddocs.sh
+++ re-builddocs.sh
@@ -3,7 +3,7 @@ set cwd "$PWD"
 cd ../HexTweaks
 hexcasting_root=../HexMod/ python3 doc/collate_data.py common/src/main/resources common/src/main/java hextweaks thetweakedbook doc/template.html doc/docs.html doc/data.json
 cd "$cwd"
-cp ../HexTweaks/doc/docs.html index.html
+mv ../HexTweaks/doc/docs.html index.html
 git add -A
 git commit -m "updated docs"
 git push
