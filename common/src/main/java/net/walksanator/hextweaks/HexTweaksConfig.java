727df15360c3548ab363862b344b30b18d2942f6
walksanatora <walkerffo22@gmail.com>
2023-05-26 15:23:41 -0700

    start working on configs

---
+++ common/src/main/java/net/walksanator/hextweaks/HexTweaksConfig.java
@@ -0,0 +1,10 @@
+package net.walksanator.hextweaks;
+
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.ConfigData;
+
+@Config(name = "hextweaks")
+public class HexTweaksConfig implements ConfigData {
+
+}
--- forge/src/main/java/net/walksanator/hextweaks/forge/HexTweaksForge.java
+++ forge/src/main/java/net/walksanator/hextweaks/forge/HexTweaksForge.java
@@ -6,8 +6,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(HexTweaks.MOD_ID)
-public class ExampleModForge {
-    public ExampleModForge() {
+public class HexTweaksForge{
+    public HexTweaksForge() {
         // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(HexTweaks.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         HexTweaks.init();
